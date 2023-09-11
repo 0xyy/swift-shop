ab9ae0e53b7c3dfd48fb3f785f0e8ab1a7fbdc8a
Tommy Phan <tommyphan2004.tp@gmail.com>
2023-09-11 20:18:19 +0200

    Fix attribute

--- src/app/components/molecules/ProductItem.tsx
+++ src/app/components/molecules/ProductItem.tsx
@@ -4,10 +4,7 @@ import { type Product } from "../types";
 
 export const ProductItem = ({ product }: { product: Product }) => {
 	return (
-		<li
-			className="p flex h-60 w-60 flex-col justify-between rounded-lg border-2 bg-slate-100 px-3 py-4"
-			data-testid="products-list"
-		>
+		<li className="p flex h-60 w-60 flex-col justify-between rounded-lg border-2 bg-slate-100 px-3 py-4">
 			<ProductItemImage {...product.coverImage} />
 			<ProductItemDescription name={product.name} price={product.price} />
 		</li>
--- src/app/components/organisms/ProductItemList.tsx
+++ src/app/components/organisms/ProductItemList.tsx
@@ -3,7 +3,7 @@ import { type Product } from "../types";
 
 export const ProductItemList = ({ products }: { products: Product[] }) => {
 	return (
-		<ul className="w-100 flex flex-grow flex-row justify-around">
+		<ul className="w-100 flex flex-grow flex-row justify-around" data-testid="products-list">
 			{products.map((product) => (
 				<ProductItem key={product.id} product={product} />
 			))}
