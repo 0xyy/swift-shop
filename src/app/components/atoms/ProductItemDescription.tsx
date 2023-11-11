4ba5b59d4a4ccdb7e6ae64a275e7ebe507485656
Tommy Phan <tommyphan2004.tp@gmail.com>
2023-11-12 00:35:22 +0100

    bf: fix temporary error

--- src/app/components/atoms/ProductItemDescription.tsx
+++ src/app/components/atoms/ProductItemDescription.tsx
@@ -1,6 +1,7 @@
 import { formatMoney } from "@/app/utils";
 import { graphql } from "@/gql";
 import { FragmentType, getFragmentData } from "@/gql/fragment-masking";
+import type { ProductListItemFragment } from "@/gql/graphql";
 
 const ProductItemDescription_Product = graphql(/* GraphQL */ `
 	fragment ProductItemDescription_Product on Product {
@@ -13,18 +14,22 @@ const ProductItemDescription_Product = graphql(/* GraphQL */ `
 `);
 
 type ProductItemDescriptionProps = {
-	product: FragmentType<typeof ProductItemDescription_Product>;
+	// product: FragmentType<typeof ProductItemDescription_Product>;
+	product: ProductListItemFragment;
 };
 
 export const ProductItemDescription = (props: ProductItemDescriptionProps) => {
-	const { name, categories, price } = getFragmentData(
-		ProductItemDescription_Product,
-		props.product,
-	);
+	// const { name, categories, price } = getFragmentData(
+	// 	ProductItemDescription_Product,
+	// 	props.product,
+	// );
+
+	const { categories, name, price } = props.product;
+
 	return (
 		<div className="flex flex-col justify-center p-4 align-middle">
 			<h3 className="text-md font-bold">{name}</h3>
-			{categories[0] && <span>{categories[0].name}</span>}
+			{categories?.[0] && <span>{categories[0].name}</span>}
 			<p className="text-sm font-semibold">{`${formatMoney(price)}`}</p>
 		</div>
 	);
